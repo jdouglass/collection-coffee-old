ab64915d64eb339306f74eae460e7be820a535d8
Johnathon <johnathondouglass@gmail.com>
2022-06-07 20:01:32 -0700

    change endpoint

--- client/src/ProductFetcher.js
+++ client/src/ProductFetcher.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'products' : 'http://localhost:5000/products'
+const baseURL = process.env.NODE_ENV === 'production' ? '/api/products' : 'http://localhost:5000/api/products'
 
 export default axios.create({
   baseURL
