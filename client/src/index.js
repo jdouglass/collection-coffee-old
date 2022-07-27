7fb071391f14dfeb74a3a1b5f2dc86a2c6e8e71b
Johnathon <johnathondouglass@gmail.com>
2022-07-26 21:04:02 -0700

    fix build

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-ReactDOM.createRoot(
+createRoot(
   document.getElementById("root")
 )
   .render(
--- server/keys.js
+++ server/keys.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB
 });
 
 module.exports = pool;
